2e6963b95066abab45b98fb72310e9b93fdf92f3
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-11-24 17:56:30 +0700

    add challenge level 1.3

---
+++ level-1-Beginner/challenge-3/main.js
@@ -0,0 +1,22 @@
+//? Challenge Level 1.3: File Content Reverser
+
+/**
+ ** Task
+ *TODO 1. Buat program yang membaca isi sebuah file teks.
+ *TODO 2. Balikkan urutan teks dalam file tersebut.
+ *TODO 3. Tampilkan teks yang dibalik di konsol.
+ */
+
+/**
+ ** Hints:
+ * Gunakan fs.readFile() untuk membaca isi file.
+ * Gunakan metode string seperti split(), reverse(), dan join() untuk membalikkan teks.
+ */
+
+
+//* Contoh output di console: !dlrow ,olleH
+
+//* here your code
+
+
+
