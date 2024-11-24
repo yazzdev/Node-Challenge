0c19d04fcf5fc70283f1528eadee99cc6f9a63fd
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-11-24 18:11:08 +0700

    add challenge level 1.6

---
+++ level-1-Beginner/challenge-6/main.js
@@ -0,0 +1,23 @@
+//? Challenge Level 1.6: Word Counter
+
+/**
+ ** Task
+ *TODO 1. Buat program yang membaca isi sebuah file teks.
+ *TODO 2. Hitung jumlah kata dalam file tersebut.
+ */
+
+/**
+ ** Hints:
+ * Gunakan fs.readFile() untuk membaca file.
+ * Pisahkan teks menjadi array kata menggunakan split().
+ */
+
+/**
+ ** Contoh output di console:
+ * Word Count: 5
+ */
+
+//* here your code
+
+
+
