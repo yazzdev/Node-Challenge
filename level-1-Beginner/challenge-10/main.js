39630bba063f5c54f34ae1a85f8ac0bf5685a768
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-11-24 18:26:57 +0700

    add challenge level 1.10

---
+++ level-1-Beginner/challenge-10/main.js
@@ -0,0 +1,30 @@
+//? Challenge Level 1.10: To-Do List
+
+/**
+ ** Task
+ *TODO 1. Buat program untuk menyimpan dan menampilkan daftar to-do.
+ *TODO 2. Tambahkan opsi untuk menambahkan tugas baru atau menampilkan daftar saat ini.
+ */
+
+/**
+ ** Hints:
+ * Simpan data to-do dalam file JSON.
+ * Gunakan fs.writeFile() untuk memperbarui file.
+ */
+
+/**
+ ** Contoh output di console:
+ * input  : Enter a new to-do: Belajar Node.js
+ * output : To-Do List:
+ * output : 1. Belajar Node.js
+ * 
+ ** output todos.js :
+ * [
+ *   "Belajar Node.js",
+ * ]
+ */
+
+//* here your code
+
+
+
