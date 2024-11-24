ada999eff2c9bf6c36bd385aa8c6e502034eae4c
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-11-24 18:05:17 +0700

    add challenge level 1.5

---
+++ level-1-Beginner/challenge-5/main.js
@@ -0,0 +1,23 @@
+//? Challenge Level 1.5: Random Quote Generator
+
+/**
+ ** Task
+ *TODO 1. Simpan beberapa kutipan motivasi dalam sebuah file.
+ *TODO 2. Buat program yang membaca file dan mencetak satu kutipan secara acak.
+ */
+
+/**
+ ** Hints:
+ * Simpan kutipan dalam format array JSON di file.
+ * Gunakan Math.random() untuk memilih kutipan secara acak.
+ */
+
+/**
+ ** Contoh output di console:
+ * Random Quote: "Success is not final, failure is not fatal: it is the courage to continue that counts."
+ */
+
+//* here your code
+
+
+
